e13845c6d15d4b934baa53824a1dabe0075baf84
AlexYalovyy <MSL5XMSdeze2BWE>
2022-03-02 20:16:20 +0200

    add step to download video

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Telegraf } = require('telegraf')
 const fs = require('fs');
+const axios = require('axios');
+
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.TOKEN)
 const dataObj = JSON.parse(fs.readFileSync("./users.json"));
 let step = 1;
-console.log("dataObj", dataObj);
 bot.start((ctx) => {
     ctx.reply("1.Введіть будь ласка своє Ім'я");
 });
@@ -27,7 +28,27 @@ bot.on("text", (ctx) => {
         ctx.reply("3.Завантажте відео");
         return;
     }
-})
+});
+
+bot.on("video_note", (ctx) => {
+    if (step !== 3) {
+        return;
+    }
+
+    const fileId = ctx.update.message.video_note.file_id;
+    ctx.telegram.getFileLink(fileId).then(url => {
+        axios({url: url.href, responseType: 'stream'}).then(response => {
+            return new Promise((resolve, reject) => {
+                response.data.pipe(fs.createWriteStream(`./videos/${ctx.update.message.from.id}.mp4`))
+                    .on('finish', () => {
+                        ctx.reply("Відео завантажене. Дякую, Ваші дані додані!");
+                    })
+                    .on('error', e => console.log(`Пом ${e}`))
+            });
+        })
+    })
+});
+
 
 bot.launch()
 
